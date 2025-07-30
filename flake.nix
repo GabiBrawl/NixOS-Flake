774fe5632d184c9c24f6d3b45a4e58b85adfdbd8
𝕲𝖆𝖇𝖎𝕭𝖗𝖆𝖜𝖑 ✨️ <gabiya219@gmail.com>
2025-07-30 15:56:05 +0100

    testgpt

--- flake.nix
+++ flake.nix
@@ -3,28 +3,38 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     hyprland.url = "github:hyprwm/Hyprland";
     hyprland.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, hyprland, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { system = "x86_64-linux"; config = { allowUnfree = true; }; };
-    in {
-      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = {
-          inherit pkgs;
-          unstable = import nixpkgs-unstable { inherit system; };
-          inherit hyprland;
-        };
-        modules = [
-          ./hosts/default.nix
-        ];
+  outputs = { self, nixpkgs, home-manager, hyprland, ... }:
+  let
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {
+        allowUnfree = true;
       };
     };
+
+    unstable = import nixpkgs {
+      inherit system;
+      overlays = [ (final: prev: {
+        # If you want to override packages or add fixes here, do it
+      }) ];
+    };
+  in {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = {
+        inherit pkgs unstable hyprland;
+      };
+      modules = [
+        ./hosts/default.nix
+      ];
+    };
+  };
 }
