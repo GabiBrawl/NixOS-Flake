53c0fffb9d4ba8b0b5354e0fa7d2fc92880adc98
𝕲𝖆𝖇𝖎𝕭𝖗𝖆𝖜𝖑 ✨️ <gabiya219@gmail.com>
2025-07-30 14:52:19 +0100

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "GabiBrawl's NixOS config with Hyprland + GNOME";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, hyprland, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+    in {
+      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = {
+          inherit pkgs;
+          unstable = import nixpkgs-unstable { inherit system; };
+          hyprland;
+        };
+        modules = [
+          ./hosts/default.nix
+        ];
+      };
+    };
+}
